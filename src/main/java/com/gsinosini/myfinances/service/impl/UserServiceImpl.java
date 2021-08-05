3ceb9019e847e17f132abb5943890ea3e2cc3c48
GiovanniSinosini <carvalho.sino@gmail.com>
2021-08-05 19:58:09 +0100

    UserService CheckforEmail method => test with JUnit5)

--- src/main/java/com/gsinosini/myfinances/service/impl/UserServiceImpl.java
+++ src/main/java/com/gsinosini/myfinances/service/impl/UserServiceImpl.java
@@ -37,9 +37,4 @@ public class UserServiceImpl implements UserService {
 		}
 	}
 	
-	
-	
-	
-	
-	
 }
---
+++ src/test/java/com/gsinosini/myfinances/model/repository/UserRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.gsinosini.myfinances.model.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.gsinosini.myfinances.model.entity.User;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public class UserRepositoryTest {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Test
+	public void CheckforEmail() {
+		
+		//context
+		User user = User.builder().name("userTest").email("userTest@email.com").build();
+		userRepository.save(user);
+		
+		// action
+		boolean result = userRepository.existsByEmail("userTest@email.com");
+		
+		// verification
+		Assertions.assertThat(result).isTrue();
+	}
+	
+	
+
+	
+	
+}
