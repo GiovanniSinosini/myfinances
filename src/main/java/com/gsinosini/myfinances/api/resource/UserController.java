4f3fec99d0244e82b122911e0f902ea5aeff3074
GiovanniSinosini <carvalho.sino@gmail.com>
2021-08-06 22:09:51 +0100

    Implementation UserController

---
+++ src/main/java/com/gsinosini/myfinances/api/resource/UserController.java
@@ -0,0 +1,53 @@
+package com.gsinosini.myfinances.api.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gsinosini.myfinances.api.dto.UserDTO;
+import com.gsinosini.myfinances.exception.BusinessRuleException;
+import com.gsinosini.myfinances.exception.ErrorAuthentication;
+import com.gsinosini.myfinances.model.entity.User;
+import com.gsinosini.myfinances.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	
+	private UserService userService;
+	
+	public UserController (UserService userService) {
+		this.userService = userService;
+	}
+	
+	@PostMapping
+	public ResponseEntity SaveUser( @RequestBody UserDTO userDto) {
+		
+		User user = User.builder()
+						.name(userDto.getName())
+						.email(userDto.getEmail())
+						.password(userDto.getPassword()).build();
+		try {
+			User userSalve = userService.userSave(user);
+			return new ResponseEntity(userSalve, HttpStatus.CREATED);
+		} catch (BusinessRuleException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@PostMapping("/authentication")
+	public ResponseEntity UserAuthentication( @RequestBody UserDTO userDto) {
+
+		try {
+			User authenticatedUser = userService.authentication(userDto.getEmail(), userDto.getPassword());
+			return ResponseEntity.ok().body(authenticatedUser);
+		
+		} catch (ErrorAuthentication e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
+	}
+}
