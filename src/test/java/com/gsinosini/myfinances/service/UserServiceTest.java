42113d004f74b2a81deacce14fcd859959e946c5
GiovanniSinosini <carvalho.sino@gmail.com>
2021-08-06 20:02:53 +0100

    UserServiceTest with Mock (@MockBean)

--- src/test/java/com/gsinosini/myfinances/service/UserServiceTest.java
+++ src/test/java/com/gsinosini/myfinances/service/UserServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -18,11 +18,12 @@ import com.gsinosini.myfinances.service.impl.UserServiceImpl;
 public class UserServiceTest {
 	
 	UserService userService;
+	
+	@MockBean
 	UserRepository userRepository;
 	
 	@BeforeEach
 	public void setUp() {
-		userRepository = Mockito.mock(UserRepository.class);
 		userService = new UserServiceImpl(userRepository);
 	}
 	
