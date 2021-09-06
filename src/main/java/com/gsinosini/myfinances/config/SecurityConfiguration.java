96a473fe7f185c34e253b5730080cadc1353cc61
GiovanniSinosini <carvalho.sino@gmail.com>
2021-09-06 22:22:54 +0100

    Disabled authentication for CreateUser and AuthenticationUser

--- src/main/java/com/gsinosini/myfinances/config/SecurityConfiguration.java
+++ src/main/java/com/gsinosini/myfinances/config/SecurityConfiguration.java
@@ -1,10 +1,12 @@
 package com.gsinosini.myfinances.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -32,7 +34,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.csrf().disable()
-			.authorizeRequests().anyRequest().authenticated()
+			.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/api/users/authentication").permitAll()
+				.antMatchers(HttpMethod.POST, "/api/users").permitAll()
+				.anyRequest().authenticated()
+		.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and()
 			.httpBasic();
 	}
